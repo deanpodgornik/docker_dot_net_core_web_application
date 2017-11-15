3b2b9e2337b8a5e16d20971fc18e3fafed6b71b8
Dean Podgornik <dean.podgornik@b-s.si>
2017-11-15 20:25:44 +0100

    Initial config

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System.Data.SqlClient;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //CONNECTION STRING TEST - BEGIN
+            SqlConnection cnn;
+            var connection = @"Server=db;Database=dean;User=sa;Password=ZacetnoGeslo2017+;";
+
+            cnn = new SqlConnection(connection);
+            try
+            {
+                cnn.Open();
+                //MessageBox.Show("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                var xxx = 1;
+                //MessageBox.Show("Can not open connection ! ");
+            }
+            //CONNECTION STRING TEST - BEGIN
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
